a5bd6cedd0f82ee77c17fcb8249fcd09832a5b52
kitan1982 <anhthao.pham@claroline.net>
2015-06-30 11:24:48 +0200

    Initial commit

---
+++ FormaLibreJobBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FormaLibre\JobBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+class FormaLibreJobBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+}
